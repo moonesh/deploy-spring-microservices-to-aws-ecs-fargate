0c51c544ad6b236d97aee7b84c7effaac31ce63b
moonesh <moonesh@havnetch.in>
2019-11-04 11:59:53 +0530

    Edited one logger file to inlcude the parameter values as well as log output

--- 04-currency-exchange-service-mysql/src/main/java/com/in28minutes/microservices/currencyexchangeservice/util/logging/EnvironmentConfigurationLogger.java
+++ 04-currency-exchange-service-mysql/src/main/java/com/in28minutes/microservices/currencyexchangeservice/util/logging/EnvironmentConfigurationLogger.java
@@ -27,7 +27,7 @@ public class EnvironmentConfigurationLogger {
 		final MutablePropertySources sources = ((AbstractEnvironment) environment).getPropertySources();
 		StreamSupport.stream(sources.spliterator(), false).filter(ps -> ps instanceof EnumerablePropertySource)
 				.map(ps -> ((EnumerablePropertySource) ps).getPropertyNames()).flatMap(Arrays::stream).distinct()
-				.forEach(prop -> LOGGER.info("{}", prop));// environment.getProperty(prop)
+				.forEach(prop -> LOGGER.info("{} - {}", prop,environment.getProperty(prop)));// environment.getProperty(prop)
 		LOGGER.info("===========================================");
 	}
 
